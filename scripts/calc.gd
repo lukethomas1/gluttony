cfe180204063e14e338e874c529cf66c4cffb601
Luke Thomas <lukethethomas@gmail.com>
2025-03-04 20:48:45 -0800

    Balancing changes

--- scripts/calc.gd
+++ scripts/calc.gd
@@ -4,13 +4,13 @@ class_name Calc
 
 const icon_width_pixels = 80
 
-const default_score:float = 30.0
-const mob_spawn_timer_ms:float = 0.2
+const default_score:float = 25.0
+const mob_spawn_timer_ms:float = 0.25
 const bomb_spawn_frequency:int = 100
 const floater_spawn_frequency:int = 25
 
 
-const mob_velocity_avg:int = 200
+const mob_velocity_avg:int = 250
 const mob_velocity_variance:int = 50
 const mob_velocity_max:int = mob_velocity_avg + mob_velocity_variance
 const mob_velocity_min:int = mob_velocity_avg - mob_velocity_variance
@@ -18,7 +18,7 @@ const mob_velocity_min:int = mob_velocity_avg - mob_velocity_variance
 
 # Calc mob and player size, based on the mob size or player score
 static func calc_scale(size:float):
-    var scale_value = max(0.5 + log(size / default_score), 0.3)
+    var scale_value = max(0.99 + log(size / default_score), 0.3)
     return Vector2(scale_value, scale_value)
 
 
@@ -34,4 +34,4 @@ static func calc_growth_amount(_player_score, _mob_size):
 
 
 static func calc_floater_growth_amount():
-    return 3
+    return 2
